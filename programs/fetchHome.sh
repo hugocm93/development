82c3414cdfecab92c258c3d33a0a2ae678461e89
Hugo Costa Machado <hmachado@tecgraf.puc-rio.br>
2020-06-16 01:29:26 -0300

    fetchhome

---
+++ programs/fetchHome.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+REMOTE="/home/p/projetos/hmachado/home"
+
+ln -sfn $REMOTE/dados $HOME/dados
+ln -sfn $REMOTE/Desktop $HOME/Desktop
+ln -sfn $REMOTE/Documents $HOME/Documents
+ln -sfn $REMOTE/Downloads $HOME/Downloads
+ln -sfn $REMOTE/Pictures $HOME/Pictures
+ln -sfn $REMOTE/projetos $HOME/projetos
