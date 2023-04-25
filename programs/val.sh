b0c83d2d9bd928dd0a420f20ca7f26343cc70f8d
Hugo Costa Machado <hmachado@tecgraf.puc-rio.br>
2023-04-25 12:50:20 -0300

    Atualizacao

--- programs/val.sh
+++ programs/val.sh
@@ -3,9 +3,9 @@
 
 VALPATH=""
 if [ $MODE = "debug" ]; then
-    VALPATH="/local/hmachado/v3o2/bin/debug/Linux64e6/v3o2"
+    VALPATH="/local/hmachado/v3o2/build/debug/Linux64e7/v3o2"
 else
-    VALPATH="/local/hmachado/v3o2/bin/release/Linux64e6/v3o2"
+    VALPATH="/local/hmachado/v3o2/build/release/Linux64e7/v3o2"
 fi
 
-vglrun valgrind --show-possibly-lost=no --leak-check=full --log-file="/local/hmachado/Desktop/logfile.out" $VALPATH
+valgrind --show-possibly-lost=no --leak-check=full --log-file="/local/hmachado/Desktop/logfile.out" $VALPATH
